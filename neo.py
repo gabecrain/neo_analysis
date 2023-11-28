7b54bcdbd50674f7e035f519025a1fe93755f757
Gabe Crain <gabecrain@Gabes-iMac.local>
2023-11-28 10:59:07 -0600

    modify data visualizations and practice adding axvline to histogram

--- neo.py
+++ neo.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import trim_mean, iqr, pearsonr, stats
 import statsmodels.api as sm
 
-neo = pd.read_csv('/Users/gabecrain/Desktop/NEO.csv')
+neo = pd.read_csv('/Users/gabecrain/Desktop/NEO_Project/NEO.csv')
 # print(neo.head())
 
 print(neo.columns.tolist())
@@ -55,62 +55,65 @@ print('\n')
 #visual analysis of miss_distance, relative_velocity, est_diameter_min, hazardous
 
 #miss_distance
-# sns.boxplot(x='miss_distance', data=neo)
-# plt.xlabel('Object Miss Distance (km)')
-# plt.title('Object Miss Distance From Earth')
+sns.boxplot(x='miss_distance', data=neo)
+plt.xlabel('Object Miss Distance (km)')
+plt.title('Object Miss Distance From Earth')
 # plt.show()
-# plt.clf()
+plt.clf()
 
-# sns.histplot(x='miss_distance', data=neo)
-# plt.xlabel('Object Miss Distance (km)')
-# plt.ylabel('Object Frequency')
-# plt.title('Relationship between Object Count and Miss Distance')
-# plt.show()
-# plt.clf()
+sns.histplot(x='miss_distance', data=neo)
+plt.xlabel('Object Miss Distance (km)')
+plt.ylabel('Object Frequency')
+plt.title('Relationship between Object Count and Miss Distance')
+plt.axvline(mean_miss_distance, color='r', linestyle='solid', linewidth=2, label='Mean')
+plt.axvline(median_miss_distance, color='y', linestyle='solid', linewidth=2, label='Median')
+plt.legend()
+plt.show()
+plt.clf()
 #the majority of the objects had a miss distance just under 40000000 kms. There was also a high concentration of objects recorded with a much shorter miss distance, bringing down the overall average.
 #it is possible this is because it is easier to observe and spot objects that are in closer proximity to the earth. Furthermore it would be prudent to study near objects that have a higher probibility of striking the earth.
 
 
 #relative_velocity
-# sns.boxplot(x='relative_velocity', data=neo)
-# plt.xlabel('Frequency of Relative Velocity')
-# plt.title('Frequency of Object Relative Velocity')
+sns.boxplot(x='relative_velocity', data=neo)
+plt.xlabel('Frequency of Relative Velocity')
+plt.title('Frequency of Object Relative Velocity')
 # plt.show()
-# plt.clf()
+plt.clf()
 
-# sns.histplot(x='relative_velocity', data=neo)
-# plt.xlabel('Object Relative Velocity (kms/hr)')
-# plt.ylabel('Frequency')
-# plt.title('Frequency of Object Relative Velocity')
+sns.histplot(x='relative_velocity', data=neo)
+plt.xlabel('Object Relative Velocity (kms/hr)')
+plt.ylabel('Frequency')
+plt.title('Frequency of Object Relative Velocity')
 # plt.show()
-# plt.clf()
+plt.clf()
 #the majority of the objects had a velocity near 35000 kms/s with some extreme outliers faster than 150000 kms/s
 
 
 # #est_diameter_min
-# neo['trimmed_diameter_min'] = neo.est_diameter_min < .1
-# sns.boxplot(x='trimmed_diameter_min', data=neo)
+neo['trimmed_diameter_min'] = neo.est_diameter_min < .1
+sns.boxplot(x='trimmed_diameter_min', data=neo)
 # plt.show()
-# plt.clf()
+plt.clf()
 # #visualization was hard to interpret due to extreme outliers. I removed the extreme 5% outliers from either side.
 
-# sns.histplot(x='trimmed_diameter_min', data=neo)
+sns.histplot(x='trimmed_diameter_min', data=neo)
 # plt.show()
-# plt.clf()
+plt.clf()
 
 
 #hazardous
-# sns.countplot(x='hazardous', data=neo)
-# plt.xlabel('Hazardous')
-# plt.ylabel('Object Count')
-# plt.title('Hazardous vs. Non-Hazardous')
+sns.countplot(x='hazardous', data=neo)
+plt.xlabel('Hazardous')
+plt.ylabel('Object Count')
+plt.title('Hazardous vs. Non-Hazardous')
 # plt.show()
-# plt.clf()
+plt.clf()
 
 # neo.hazardous.value_counts().plot.pie(autopct='%.1f')
-# plt.title('Number of Objects Considered Hazardous')
+plt.title('Number of Objects Considered Hazardous')
 # plt.show()
-# plt.clf()
+plt.clf()
 #the majority of neo objects in this dataset are not considered hazardous, with less than 10% of entries labeled as hazardous.
 
 
@@ -136,22 +139,22 @@ print('difference between median velocity of objects classified as hazardous vs 
 print('\n')
 #there seems to be a noticable difference between velocities of objects classified as hazardous vs non hazardous
 
-# sns.boxplot(data=neo, x='hazardous', y='relative_velocity')
-# plt.xlabel('Hazardous')
-# plt.ylabel('Relative Velocity (kms/hr)')
-# plt.title('Relationship Between Object Hazardousness and Relative Velocity')
+sns.boxplot(data=neo, x='hazardous', y='relative_velocity')
+plt.xlabel('Hazardous')
+plt.ylabel('Relative Velocity (kms/hr)')
+plt.title('Relationship Between Object Hazardousness and Relative Velocity')
 # plt.show()
-# plt.clf()
+plt.clf()
 #boxplot shows only partial overlap between the two variables, further indicating variance between the two.
 
-# plt.hist(true_hazardous, color='blue', label='True', density=True, alpha=.5)
-# plt.hist(false_hazardous, color='red', label='False', density=True, alpha=.5)
-# plt.legend()
-# plt.xlabel('Hazardous')
-# plt.ylabel('Relative Velocity (kms/hr)')
-# plt.title('Relationship between Object Hazardousness and Relative Velocity')
+plt.hist(true_hazardous, color='blue', label='True', density=True, alpha=.5)
+plt.hist(false_hazardous, color='red', label='False', density=True, alpha=.5)
+plt.legend()
+plt.xlabel('Hazardous')
+plt.ylabel('Relative Velocity (kms/hr)')
+plt.title('Relationship between Object Hazardousness and Relative Velocity')
 # plt.show()
-# plt.clf()
+plt.clf()
 #histogram indicates same as boxplot
 
 
@@ -172,8 +175,7 @@ plt.savefig('neo_velocity_diameter.png')
 
 predicted_velocity = 18773 * neo.est_diameter_min + 45675
 plt.plot(neo.est_diameter_min, predicted_velocity)
-
-plt.show()
+# plt.show()
 plt.clf()
 #results are hard for me to interpret, I will next try linear regression to further explore the relationship between the two variables.
 
@@ -187,17 +189,17 @@ fitted_values = model_results.predict(neo)
 residuals = neo.relative_velocity - fitted_values
 
 #verify that the residuals are normally distributed
-# plt.hist(residuals, bins=50)
-# plt.xlim(-100000, 100000)
+plt.hist(residuals, bins=50)
+plt.xlim(-100000, 100000)
 # plt.show()
-# plt.clf()
+plt.clf()
 
 #verify that the residuals have equal variation
-# plt.scatter(fitted_values, residuals)
-# plt.xlim(40000, 100000)
-# plt.ylim(-100000, 150000)
+plt.scatter(fitted_values, residuals)
+plt.xlim(40000, 100000)
+plt.ylim(-100000, 150000)
 # plt.show()
-# plt.clf()
+plt.clf()
 #there does not appear to be any patterns or asymmetry to the data.
 
 #find covariance and correlation between relative velocity and minimum estimated diameter
